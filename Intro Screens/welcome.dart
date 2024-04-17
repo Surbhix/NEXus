291495aefb87f2c458f74f46cb4540b84aef88b8
Surbhix <165020956+Surbhix@users.noreply.github.com>
2024-04-17 21:40:36 +0530

    Add files via upload

---
+++ Intro Screens/welcome.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:nexus/home.dart';
+
+// ignore: camel_case_types
+class welcome extends StatelessWidget {
+  const welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+     home: Scaffold(
+      appBar:  AppBar(title: const Text("                                 NEXus"), backgroundColor: const Color.fromARGB(255, 79, 40, 91)),
+     backgroundColor:const Color.fromARGB(255, 252, 246, 246), 
+    body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 35, top: 200),
+              child: const Text(
+                'Welcome!',
+                style: TextStyle(color:const  Color.fromARGB(255, 79, 40, 91), fontSize: 60, fontWeight: FontWeight.w700),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.43),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin:const EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+                          TextField(
+                            style: const TextStyle(color: Colors.black),
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Email",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            style: const TextStyle(),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: "Password",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                               TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const home()));
+                                },
+                              
+                             child: const Text(
+                                'Sign in',
+                                style: TextStyle( color:  Color.fromARGB(255, 79, 40, 91),
+                                    fontSize: 27, fontWeight: FontWeight.w700),
+                              ),
+                               )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const home()));
+                                },
+                                child: const Text(
+                                  'Sign Up',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                      fontSize: 18),
+                                ),
+                                style: const ButtonStyle(),
+                              ),
+                              TextButton(
+                                  onPressed: () { Navigator.of(context).push(MaterialPageRoute(builder: (context) => const home()));},
+                                  child: const Text(
+                                    'Forgot Password',
+                                    style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Color(0xff4c505b),
+                                      fontSize: 18,
+                                    ),
+                                  )),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+     ),
+   );
+  }
+}
